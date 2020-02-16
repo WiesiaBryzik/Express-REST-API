2ed7ac1b3d2be46ac6e266a259063ecfdeaebb2b
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-02-16 20:11:11 +0100

    Add endpoints get post put delete

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ app.get('/testimonials', (req, res) => {
     res.json(db);
 });
 
+app.get('/testimonials/random', (req, res) => {
+    const random = db[Math.floor(Math.random() * db.length)];
+    res.json(random);
+});
+
 app.get('/testimonials/:id', (req, res) => {
     const user = db.find(u => u.id == req.params.id)
     res.json(user);
-});
-
-app.get('/testimonials/random', (req, res) => {
-    const random = db[Math.floor(Math.random * db.length)];
-    res.json(random);
 });
 
 app.post('/testimonials', (req, res) => {
@@ -25,6 +25,31 @@ app.post('/testimonials', (req, res) => {
         id = uuidv4();
     const newElement = { id: id, author: author, text: text };
     db.push(newElement);
+    res.json({ message: 'OK' });
+  });
+
+  app.put('/testimonials/:id', (req, res) => {
+    const { author, text } = req.body;
+    const id = req.params.id;
+    const newElement = { id: id, author: author, text: text };
+    
+    for (let element of db) {
+        if (element.id == id) {
+            db.splice(db.indexOf(element), 1, newElement);
+        }
+    }
+    res.json({ message: 'OK' });
+  });
+
+  app.delete('/testimonials/:id', (req, res) => {
+    const id = req.params.id;
+    
+    for (let element of db) {
+        if (element.id == id) {
+            db.splice(db.indexOf(element), 1);
+        }
+    }
+    res.json({ message: 'OK' });
   });
 
 app.listen(8000, () => {
