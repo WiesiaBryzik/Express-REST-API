d12c14615683b4bf329254fc2292a559b52ba8f0
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-02-16 18:17:56 +0100

    Add changes

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const app = express();
+const uuidv4 = require('uuid/v4');
+const db = require('./db');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/testimonials', (req, res) => {
+    res.json(db);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    const user = db.find(u => u.id == req.params.id)
+    res.json(user);
+});
+
+app.get('/testimonials/random', (req, res) => {
+    const random = db[Math.floor(Math.random * db.length)];
+    res.json(random);
+});
+
+app.post('/testimonials', (req, res) => {
+    const { author, text } = req.body;
+        id = uuidv4();
+    const newElement = { id: id, author: author, text: text };
+    db.push(newElement);
+  });
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
