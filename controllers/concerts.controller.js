91f97ea88f91b8849342ab1624a9b08211c64188
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-03-07 20:32:32 +0100

    Add endpoints

--- controllers/concerts.controller.js
+++ controllers/concerts.controller.js
@@ -1,67 +1,164 @@
 const Concert = require('../models/concerts.model');
 
 exports.getAll = async (req, res) => {
-    try {
-      res.json(await Concert.find());
-    }
-    catch (err) {
-      res.status(500).json(err)
-    }
-  };
+  try {
+    res.json(await Concert.find());
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
+};
 
-  exports.getId = async (req, res) => {
+exports.getId = async (req, res) => {
 
-    try {
-      const dep = await Concert.findById(req.params.id);
-      if (!dep) res.status(404).json({ message: 'Not found' });
-      else res.json(dep);
+  try {
+    const dep = await Concert.findById(req.params.id);
+    if (!dep) res.status(404).json({ message: 'Not found' });
+    else res.json(dep);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+
+exports.getPerformer = async (req, res) => {
+
+  try {
+    const per = await Concert.find();
+
+    if (!per) res.status(404).json({ message: 'Not found' });
+    else {
+      const performerArray = []
+      for (let p of per) {
+        if (p.performer === req.params.performer) {
+          performerArray.push(p);
+        }
+      }
+      res.json(performerArray);
     }
-    catch (err) {
-      res.status(500).json(err);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getGenre = async (req, res) => {
+
+  try {
+    const per = await Concert.find();
+
+    if (!per) res.status(404).json({ message: 'Not found' });
+    else {
+      const genreArray = []
+      for (let p of per) {
+        if (p.genre === req.params.genre) {
+          genreArray.push(p);
+        }
+      }
+      res.json(genreArray);
     }
-  };
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-  exports.postNew = async (req, res) => {
+exports.getDay = async (req, res) => {
 
-    try {
-  
-      const { performer, genre, price, day, image } = req.body;
-      const newConcert = new Concert({ performer: performer, genre: genre, price: price, day: day, image: image });
-      await newConcert.save();
-      res.json({ message: 'OK' });
-  
-    } catch (err) {
-      res.status(500).json(err);
+  try {
+    const per = await Concert.find();
+
+    if (!per) res.status(404).json({ message: 'Not found' });
+    else {
+      const dayArray = []
+      for (let p of per) {
+        if (p.day === parseInt(req.params.day)) {
+          dayArray.push(p);
+        }
+      }
+      res.json(dayArray);
     }
-  };
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-  exports.putId = async (req, res) => {
+exports.getPrice = async (req, res) => {
+
+  try {
+    const per = await Concert.find();
+
+    if (!per) res.status(404).json({ message: 'Not found' });
+    else {
+      const priceMin = parseInt(req.params.price_min);
+      const priceMax = parseInt(req.params.price_max);
+
+      const priceArray = []
+      for (let p of per) {
+        if ( p.price >= priceMin && p.price <= priceMax) {
+          priceArray.push(p);
+        }
+      }
+      res.json(priceArray);
+    }
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+exports.postNew = async (req, res) => {
+
+  try {
+
     const { performer, genre, price, day, image } = req.body;
-  
-    try {
-      const dep = await (Concert.findById(req.params.id));
-      if (dep) {
-        await Concert.updateOne({ _id: req.params.id }, { $set: { performer: performer, genre: genre, price: price, day: day, image: image } });
-        res.json({ message: 'OK' + ' updated dataBase:' + await Concert.find() });
-      }
-      else res.status(404).json({ message: 'Not found...' });
-    }
-    catch (err) {
-      res.status(500).json(err);
-    }
-  };
+    const newConcert = new Concert({ performer: performer, genre: genre, price: price, day: day, image: image });
+    await newConcert.save();
+    res.json({ message: 'OK' });
 
-  exports.deleteId = async (req, res) => {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
 
-    try {
-      const dep = await (Concert.findById(req.params.id));
-      if (dep) {
-        await Concert.deleteOne({ _id: req.params.id });
-        res.json({ message: 'OK' + ' deleted element' + dep + 'updated dataBase:' + await Concert.find() });
-      }
-      else res.status(404).json({ message: 'Not found...' });
+exports.putId = async (req, res) => {
+  const { performer, genre, price, day, image } = req.body;
+
+  try {
+    const dep = await (Concert.findById(req.params.id));
+    if (dep) {
+      await Concert.updateOne({ _id: req.params.id }, { $set: { performer: performer, genre: genre, price: price, day: day, image: image } });
+      res.json({ message: 'OK' + ' updated dataBase:' + await Concert.find() });
     }
-    catch (err) {
-      res.status(500).json(err);
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.deleteId = async (req, res) => {
+
+  try {
+    const dep = await (Concert.findById(req.params.id));
+    if (dep) {
+      await Concert.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' + ' deleted element' + dep + 'updated dataBase:' + await Concert.find() });
     }
-  };
+    else res.status(404).json({ message: 'Not found...' });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -9,6 +9,9 @@ router.post('/concerts', ConcertController.postNew);
 router.put('/concerts/:id', ConcertController.putId);
 router.delete('/concerts/:id', ConcertController.deleteId);
 
-
+router.get('/concerts/performer/:performer', ConcertController.getPerformer);
+router.get('/concerts/genre/:genre', ConcertController.getGenre);
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getPrice);
+router.get('/concerts/price/day/:day', ConcertController.getDay);
 
 module.exports = router;
